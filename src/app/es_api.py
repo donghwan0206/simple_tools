2a17d3db92e739ec91c50ca7dcb6f10c2e04bdc4
donghwan <donghwan0206@gmail.com>
2024-05-03 16:25:39 +0900

    es_api 추가

--- src/app/es_api.py
+++ src/app/es_api.py
@@ -14,6 +14,49 @@ def get_aliases_via_index_name(index_name, es_url):
         return False, resp["error"]
 
 
+def get_all_aliases(es_url):
+    import pandas as pd
+
+    end_point = f"{es_url}/_cat/aliases?format=json&s=index:desc"
+
+    resp = requests.get(end_point)
+
+    if resp.status_code == 200:
+        return True, {
+            alias: group["index"].to_list()
+            for alias, group in pd.DataFrame(resp.json()).groupby("alias")
+            if not alias.startswith(".")
+        }
+    else:
+        return False, resp
+
+
+def get_indices_via_phrase(phrase, es_url):
+    import pandas as pd
+
+    end_point = f"{es_url}/_cat/indices/{phrase}?format=json&s=index:desc"
+
+    resp = requests.get(end_point)
+
+    if resp.status_code == 200:
+        return True, pd.DataFrame(resp.json())["index"].to_list()
+    else:
+        return False, resp
+
+
+def get_all_indices(es_url):
+    import pandas as pd
+
+    end_point = f"{es_url}/_cat/indices?format=json&s=index:desc"
+
+    resp = requests.get(end_point)
+
+    if resp.status_code == 200:
+        return True, pd.DataFrame(resp.json())["index"].to_list()
+    else:
+        return False, resp
+
+
 def change_aliases_old_to_new(old_index, new_index, aliases, es_url):
     end_point = f"{es_url}/_aliases"
     headers = {"Content-Type": "application/json; charset=utf-8"}
@@ -44,18 +87,5 @@ def change_aliases_old_to_new(old_index, new_index, aliases, es_url):
 
     if resp.status_code == 200:
         return True, resp
-    else:
-        return False, resp
-
-
-def get_indices(es_url):
-    import pandas as pd
-
-    end_point = f"{es_url}/_cat/indices?format=json&s=index:desc"
-
-    resp = requests.get(end_point)
-
-    if resp.status_code == 200:
-        return True, pd.DataFrame(resp.json())["index"].to_list()
     else:
         return False, resp
